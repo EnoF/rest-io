d8f90c513cedec091ebc99bf43f8f0d53a910264
Andy Tang <andy.tang@jumio.com>
2015-06-14 20:02:24 +0200

    added decryption of auth token functionality

--- src/authentication.ts
+++ src/authentication.ts
@@ -1,4 +1,5 @@
 module authentication {
+  var CryptoJS = require('crypto-js');
   var HMAC = require('crypto-js/hmac-sha256');
   var AES = require('crypto-js/aes');
 
@@ -10,6 +11,18 @@ module authentication {
     var authToken = AES.encrypt(userId + ';' +
       new Date().getTime(), process.env.REST_IO_AES_KEY);
     return authToken.toString();
+  }
+
+  export function decryptAuthToken(authToken: string) {
+    var decryptedMessage = AES.decrypt(authToken, process.env.REST_IO_AES_KEY);
+    var brokenMessage = decryptedMessage.toString(CryptoJS.enc.Utf8).split(';');
+    if (brokenMessage.length !== 2) {
+      throw new Error('corrupt auth token');
+    }
+    return {
+      id: brokenMessage[0],
+      createdAt: new Date(parseInt(brokenMessage[1], 10))
+    };
   }
 }
 
